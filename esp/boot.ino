c0db3e5528dd636dbeb6304df2f229147af014e0
pizadi <parham.avia@gmail.com>
2023-05-04 15:44:53 +0330

    Added basic state

--- esp/boot.ino
+++ esp/boot.ino
@@ -7,13 +7,25 @@ extern byte networkID[4];
 extern byte AESKey[16];
 
 int func_boot() {
-  EEPROM.begin(EEPROM_SIZE);
+  if (EEPROM.begin(EEPROM_SIZE) == 0) {
+    Serial.println("Fatal Error: Failed to initiate EEPROM.");
+    return STATE_SLEEP;
+  }
   if (EEPROM.readByte(0) == 0x67) {
-    
+    if (EEPROM.readBytes(1, AESKey, 16) == 16 && EEPROM.readBytes(17, networkID, 4) == 4) {
+      Serial.println();
+    }
+    else {
+      Serial.println("Fatal Error: Device EEPROM corrupted.");
+      return STATE_SLEEP;
+    }
   }
   else
   {
+    
+    Serial.println("Error: No configuratoin found.");
     return STATE_SLEEP;
   }
+  EEPROM.end();
   return STATE_BOOT;
 }
