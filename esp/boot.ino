fb895a388a41a6341c2952006d3baf0125310246
pizadi <parham.avia@gmail.com>
2023-05-04 15:42:15 +0330

    Added base states

--- esp/boot.ino
+++ esp/boot.ino
@@ -1,4 +1,19 @@
 #include "states.h"
+#include <EEPROM.h>
+
+#define EEPROM_SIZE 21
+
+extern byte networkID[4];
+extern byte AESKey[16];
+
 int func_boot() {
+  EEPROM.begin(EEPROM_SIZE);
+  if (EEPROM.readByte(0) == 0x67) {
+    
+  }
+  else
+  {
+    return STATE_SLEEP;
+  }
   return STATE_BOOT;
 }
--- esp/states.ino
+++ esp/states.ino
@@ -1,8 +1,10 @@
 #ifndef STATES_H
 #define STATES_H
 
+#define STATE_SLEEP 0
 #define STATE_BOOT 1
 
+int func_sleep();
 int func_boot();
 
 #endif
