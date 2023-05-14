c182ebf5a3045ca566956cefd021bc784090df12
pizadi <parham.avia@gmail.com>
2023-05-14 12:25:03 +0330

    Updated packet structure

--- esp/headers.h
+++ esp/headers.h
@@ -6,7 +6,7 @@
 #define HEADER_ACK 1 << 2
 #define HEADER_NACK 1 << 3
 #define HEADER_CMD 1 << 4
-#define HEADER_SRC 1 << 5
+#define HEADER_SRCH 1 << 5
 #define HEADER_ADP 1 << 6
 
 #define HEADER_ANY 0xFF
